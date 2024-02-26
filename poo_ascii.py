d8448cdc601fb1bdcc9ee9c07f6a8851e5d5abf1
DaniKaganovich <svinozhop@gmail.com>
2024-02-26 17:28:42 +0200

    Fix bug in ASCII art generation

---
+++ poo_ascii.py
@@ -0,0 +1,17 @@
+
+# poo_ascii.py
+
+def print_poo_ascii():
+    poo_ascii = [
+        "  _____ ",
+        " |  |  |",
+        " |  |  |",
+        " |  |  |",
+        " |__|__|"
+    ]
+
+    for line in poo_ascii:
+        print(line)
+
+if __name__ == "__main__":
+    print_poo_ascii()
